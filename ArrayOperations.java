91ab4234252c608cc203f8688bb4d6088c086ba5
sahumahak01 <mahak12607@gmail.com>
2026-06-19 17:01:59 +0530

    Add array processing methods with sorting functionality
    Implemented bubble sort and methods to find second highest and second lowest numbers in an array.

---
+++ ArrayOperations.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class ArrayOperations {
+
+    // Method to sort array and find second highest & second lowest
+    public static void processArray(int[] arr) {
+
+        // Bubble Sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nSorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        int secondLowest = arr[1];
+        int secondHighest = arr[arr.length - 2];
+
+        System.out.println("\nSecond Lowest Number: " + secondLowest);
+        System.out.println("Second Highest Number: " + secondHighest);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 different numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        processArray(numbers);
+
+        sc.close();
+    }
+}
